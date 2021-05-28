d602142bb5a2508ba799a813920c834a4c9abde0
EC2 Default User <d.yoshida1352@gmail.com>
2021-05-28 13:50:32 +0000

    [Add]skill up 学習機能

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -13,28 +13,32 @@ class TodolistsController < ApplicationController
   end
 
   def index
+    puts "作成したキー #{ENV['SECRET_KEY']}"
     @lists = List.all
   end
 
   def show
     @list = List.find(params[:id])
   end
+
   def edit
     @list = List.find(params[:id])
   end
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  def destroy
-    list = List.find(params[:id])  # データ（レコード）を1件取得
-    list.destroy  # データ（レコード）を削除
-    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
 
+  def destroy
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト
   end
 
   private
+
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body, :image)
